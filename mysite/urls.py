156c66f2d869cfa01a0eecbe59d2b1abb48938f5
Korneliusz <rzeczyckikornel@gmail.com>
2019-08-20 22:38:39 +0200

    Changed urls.py no 1

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
